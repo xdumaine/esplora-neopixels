10bfa719d100824bffd60d6ced62d0b2e0927612
Xander Dumaine <xander.dumaine@inin.com>
2015-11-01 21:05:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+###Esplora Neopixels
+
+This is a build for using the Esplora Arduino board with a Neopixels LED strip.
+The goal is to mount the build on my motorcycle for fun LED effects while
+riding.
+
+####License
+MIT
---
+++ esplora-neopixels.ino
@@ -0,0 +1,41 @@
+/*
+  Esplora controller for Neopixels strip using sensors and inputs
+  to change strip colors and brightness for use on a motorcycle
+*/
+
+#include <Adafruit_NeoPixel.h>
+#include <Esplora.h>
+
+#define PIN     11
+#define N_LEDS  30
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // initialize the serial communication:
+  Serial.begin(9600);
+
+  strip.begin();
+  strip.show();
+  strip.setBrightness(64);
+}
+
+void loop() {
+  int ambientLight = Esplora.readLightSensor();
+  if (ambientLight > 700) {
+    strip.setBrightness(0);
+  } else {
+    int xAxis = Esplora.readAccelerometer(X_AXIS);
+    int yAxis = Esplora.readAccelerometer(Y_AXIS);
+    int zAxis = Esplora.readAccelerometer(Z_AXIS);
+    for(int i = 0; i < N_LEDS; i++) {
+      strip.setPixelColor(i, xAxis, yAxis, zAxis);
+    }
+    int brightness = Esplora.readMicrophone()/4;
+    strip.setBrightness(brightness);
+  }
+  strip.show();
+
+  // add a delay to keep the LED from flickering:
+  delay(10);
+}
